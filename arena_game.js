ad2bb2a29355fea913a06e7051f280e3de1a6a14
Ishank Bansal <ishankbansal1111@gmail.com>
2024-01-26 17:04:50 +0530

    feat: intialise players with their properties using player class

---
+++ arena_game.js
@@ -0,0 +1,12 @@
+class player {
+    constructor(health, strength, attack){
+        this.health = health;
+        this.strength = strength;
+        this.attack = attack;
+    }
+}
+
+// initialisation of players
+const playerA = new player(50, 5, 10)
+const playerB = new player(100, 10, 5)
+
